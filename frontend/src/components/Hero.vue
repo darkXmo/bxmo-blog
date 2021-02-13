819ab8f51ef1faae913e035fac9f0b72e287469d
dXmo <188551989097@163.com>
2021-02-13 20:21:47 +0800

    chore: pretty code

--- frontend/src/components/Hero.vue
+++ frontend/src/components/Hero.vue
@@ -2,8 +2,8 @@
   <div class="hero flex-align-center">
     <div class="title-motto">
       <h1>Blog of Xmo</h1>
-      <h3>乱石穿空，惊涛拍岸，卷起千堆雪</h3>
-      <h3>江山如画，一时多少豪杰</h3>
+      <h2>乱石穿空，惊涛拍岸，卷起千堆雪</h2>
+      <h2>江山如画，一时多少豪杰</h2>
     </div>
     <!-- <a href="https://www.pixiv.net/artworks/68818298" class="avatar">
       <img src="@/assets/img/LS-full.jpeg" alt="头像 @wube+岚少" title="@wube + 岚少，点击跳转作品源链接">
