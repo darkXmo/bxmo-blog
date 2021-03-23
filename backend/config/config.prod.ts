c8b74e506d7d4224ea4a0b7fab1802f0afe69192
Xmo <18851989097@163.com>
2021-03-24 03:30:40 +0800

    ci(config): 添加https配置

--- backend/config/config.prod.ts
+++ backend/config/config.prod.ts
@@ -1,7 +1,14 @@
 import { EggAppConfig, PowerPartial } from 'egg';
+import path = require('path');
 
 export default () => {
   const config: PowerPartial<EggAppConfig> = {};
+  config.cluster = {
+    https: {
+      key: path.join(__dirname, './5370296_bxmoback.top.key'), // https 证书绝对目录
+      cert: path.join(__dirname, './5370296_bxmoback.top.crt'), // https 证书绝对目录
+    },
+  };
   const mysql = {
     client: {
       host: 'localhost',
